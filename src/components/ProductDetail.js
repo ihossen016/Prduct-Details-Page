a743b74c1a902feb401c57ce4ea15e64afe97172
ihossen016 <ihossen016@gmail.com>
2022-06-06 23:41:31 +0600

    product detail section fixed

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,63 +1,83 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { ProductContext } from "../context/Context";
 
 const ProductDetail = () => {
   const { productDetails } = useContext(ProductContext);
 
-  // const percent = ((price.old - price.discounted) / price.old) * 100;
+  const [discountedPrice, setDiscountedPrice] = useState();
+  const [oldPrice, setOldPrice] = useState();
+  const [colorList, setColorList] = useState([]);
+  const [sizeList, setSizeList] = useState([]);
+  const [colorName, setColorName] = useState("");
+  const [sizeNum, setSizeNum] = useState("");
 
-  // const colorList = colors.values;
-  // const sizeList = sizes.values;
+  // extracting data from productDetails
+  useEffect(() => {
+    setDiscountedPrice(productDetails && productDetails.price.discounted);
+    setOldPrice(productDetails && productDetails.price.old);
+    setColorList(
+      productDetails && [...productDetails.variation.props[0].values]
+    );
+    setSizeList(
+      productDetails && [...productDetails.variation.props[1].values]
+    );
 
-  // const [colorName, setColorName] = useState(colorList[0].name);
-  // const [sizeName, setSizeName] = useState(sizeList[0].name);
+    setColorName(
+      productDetails && productDetails.variation.props[0].values[0].name
+    );
+    setSizeNum(
+      productDetails && productDetails.variation.props[1].values[0].name
+    );
+  }, [productDetails]);
 
-  // const [selectedColor, setSelectedColor] = useState(colors[0].img);
-
-  // console.log(colorList);
-  // console.log(sizeList);
+  const percent = ((oldPrice - discountedPrice) / oldPrice) * 100;
 
   return (
     <div className="product-detail">
       <div className="product-container">
         <div className="product-info">
           <h2>{productDetails && productDetails.title}</h2>
-          {/* <h4>
-            Price: ${price.discounted} <span>${price.old}</span>
-            <small>({percent.toFixed(0)}% Discount)</small>
-          </h4> */}
+          {productDetails && (
+            <h4>
+              Price: ${discountedPrice}
+              <span>${oldPrice}</span>
+              <small>({percent.toFixed(0)}% Discount)</small>
+            </h4>
+          )}
         </div>
         <div className="color">
-          {/* <h4>Color: {colorName}</h4>
+          <h4>Color: {colorName}</h4>
           <div className="color-imgs">
-            {colorList.map(color => (
-              <img
-                key={color.id}
-                src={color.image}
-                alt="Shoe Color"
-                style={{
-                  border: colorName === color.name ? "4px solid orange" : "",
-                }}
-                onClick={() => setColorName(color.name)}
-              />
-            ))}
-          </div> */}
+            {colorList &&
+              colorList.map(color => (
+                <img
+                  key={color.id}
+                  src={color.image}
+                  alt="Shoe Color"
+                  style={{
+                    border: colorName === color.name ? "3px solid orange" : "",
+                  }}
+                  onClick={() => setColorName(color.name)}
+                />
+              ))}
+          </div>
         </div>
         <div className="size">
-          {/* <h4>Size: {sizeName}</h4>
+          <h4>Size: {sizeNum}</h4>
           <div className="shoe-sizes">
-            {sizeList.map(size => (
-              <li
-                key={size.id}
-                style={{
-                  border: sizeName === size.name ? "4px solid orange" : "",
-                }}
-                onClick={() => setSizeName(size.name)}
-              >
-                {size.name}
-              </li>
-            ))}
-          </div> */}
+            {sizeList &&
+              sizeList.map(size => (
+                <li
+                  key={size.id}
+                  style={{
+                    border: sizeNum === size.name ? "3px solid orange" : "",
+                  }}
+                  onClick={() => setSizeNum(size.name)}
+                >
+                  {size.name}
+                </li>
+              ))}
+          </div>
         </div>
         <button className="cart-btn">Add to Cart</button>
       </div>
