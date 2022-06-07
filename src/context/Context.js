67be60570b4bf24b8aeeadd0e9893a1949b5fa4e
ihossen016 <ihossen016@gmail.com>
2022-06-07 12:12:28 +0600

    file cleaned

--- src/context/Context.js
+++ src/context/Context.js
@@ -16,33 +16,6 @@ const Context = ({ children }) => {
       .then(data => setProductDetails(data));
   }, []);
 
-  // const [gallery, setGallery] = useState([]);
-  // const [titleImg, setTitleImg] = useState("");
-  // const [title, setTitle] = useState("");
-  // const [price, setPrice] = useState({});
-  // const [colors, setColors] = useState([]);
-  // const [sizes, setSizes] = useState([]);
-  // // const [skus, setSkus] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(
-  //     "https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product"
-  //   )
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       setGallery(data.gallery.map(img => img.url));
-  //       setTitleImg(data.image);
-  //       setTitle(data.title);
-  //       setPrice(data.price);
-  //       setColors(data.variation.props[0]);
-  //       setSizes(data.variation.props[1]);
-  //       // setSkus(data.variation.skus);
-  //       console.log(data);
-  //     });
-  // }, []);
-
-  //   console.log(gallery, title, price);
-
   return (
     <ProductContext.Provider
       value={{ productDetails, selectedImg, setSelectedImg }}
